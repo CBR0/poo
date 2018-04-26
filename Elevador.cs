e107cf04bae3479fb32b57fdf0a964d51c1f5d1d
Cléber Otaviano <cleberotaviano.core@gmail.com>
2018-04-26 20:25:58 -0300

    Classe elevador

---
+++ Elevador.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Elevador
+    {
+        private int andarAtual;
+        private int totalAndares;
+        private bool estadoPorta;
+
+
+        private void Subir(int andarDesejado)
+        {
+            while (andarAtual < andarDesejado) 
+            {
+                andarAtual++;
+            }
+        }
+        private void Descer(int andarDesejado)
+        {
+            while (andarAtual < andarDesejado)
+            {
+                andarAtual--;
+            }
+            //AbrirPorta();
+        }
+        private void AbrirPorta()
+        {
+            estadoPorta = true;
+        }
+        private void FecharPorta()
+        {
+            estadoPorta = false;
+        }
+        public void irParaAndar(int andarDesejado)
+        {
+            if (andarDesejado > andarAtual && andarDesejado < totalAndares)
+            {
+                Subir(andarDesejado);
+            }
+            else
+            {
+                Descer(andarDesejado);
+            }
+            
+        }
+    }
+}
